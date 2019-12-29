16ebb56ff88fff708eae1080ab6eab1a074c908c
MinexAutomation <minex.automation@gmail.com>
2019-12-29 02:07:10 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Norwich.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Norwich.Newport;
+
+
+namespace R5T.Norwich.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJsonFileSerializer(this IServiceCollection services)
+        {
+            services.AddSingleton<IJsonFileSerializer, NewtonsoftJsonFileSerializer>();
+
+            return services;
+        }
+    }
+}
